6cdbcf0878f5c5649e882d97b185a71576f5cfa0
TilCreator <tilcreator@tc-j.de>
2020-01-28 18:00:08 +0100

    Fix excption not excepting

--- tapas-dl.py
+++ tapas-dl.py
@@ -18,7 +18,7 @@ def terminal_size():
         import struct
 
         th, tw, hp, wp = struct.unpack('HHHH', fcntl.ioctl(0, termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0, 0)))
-    except IOError or ModuleNotFoundError:
+    except (IOError, ModuleNotFoundError):
         th, tw = 80, 200
     return tw, th
 
